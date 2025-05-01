ddb84be2880c4214767ab3aac0514c4c0b19de9c
Ensor Palacios <yq18021@bristol.ac.uk>
2025-05-01 11:07:49 +0100

    add src/colour-mapping.R: contains model-colour mapping

---
+++ src/colour-mapping.R
@@ -0,0 +1,30 @@
+#' model colours
+#' 
+#' Define colour mapping for different models.
+#' 
+#' @author Ensor Palacios, email{ensorrafael.palacios@bristol.ac.uk}
+#' @date 2025-04-24
+
+# Import packages --------------------------------------------------------------
+library(scales)
+
+
+# Define colour mapping --------------------------------------------------------
+# Alternative colour scales
+# show_col(rainbow(12))
+# show_col(hue_pal()(12))
+# show_col(viridis_pal(option="turbo")(12))
+
+col_models = viridis_pal(option = "turbo")(12) # Colour mapping
+names(col_models) = c("arima", 
+                      "arima_d",
+                      "arima_dad",
+                      "arima_de",
+                      "arima_dade",
+                      "es_e",
+                      "mean",
+                      "naive",
+                      "snaive",
+                      "naive",
+                      "baseline_min",
+                      "wilker_min")
