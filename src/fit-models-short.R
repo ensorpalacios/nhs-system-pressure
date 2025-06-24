510a5727301997c1293621e462b6944f1bf00732
Ensor Palacios <yq18021@bristol.ac.uk>
2025-06-24 14:00:29 +0100

    modified src/fit-models-short.R: added VAM, reconciled, ad_diff2/3_f, occ_other

--- src/fit-models-short.R
+++ src/fit-models-short.R
@@ -17,79 +17,216 @@ source("src/split-data.R")
 
 
 # Load data --------------------------------------------------------------------
-data_path <- paste0(here(), "/data/processed/bed_occupancy.RDS")
+data_path <- paste0(here(), "/data/processed/tbl_occ.RDS")
 ts_occ <- readRDS(file = data_path)
-ts_occ <- # drop original data with missing values
-  ts_occ %>% select(-(ts_occ %>% names %>% grep("_m", .)))
 sites <- ts_occ$site |> unique()
 
 
+
+# Select relevant variables ----------------------------------------------------
+ts_occ <- 
+  ts_occ %>%
+  select(
+    -(ts_occ %>% names %>% grep("_m", .)), # original data with missing values
+    -adm, -dis,
+    -escal, -core,
+    # -escal, -core, -occ_i,
+    # -ad_diff, -ad_diff2, -ad_diff3
+    )
+
+
+
 # Lag/split dataset ------------------------------------------------------------
-ts_occ_lag <- lag_fun(ts_occ) # lag data
+horizon = 7
+ts_occ_lag <- lag_fun(ts_occ, .lag = horizon) # lag data
 
 split_data_tt <- # Train/test set
   split_tt(ts_occ_lag)
 
 initial <- "16 weeks" 
-assess <- "2 weeks"
-skip <- "8 weeks"
+assess <- "1 weeks"
+skip <- "6 weeks"
 split_data_cv <- # Cv train/validation sets
   split_cv(split_data_tt, initial, assess, skip)
 
 splits <- split_data_cv$split %>% unique() # save cv splits names
+idx_start_test <- split_data_cv$type %>% grep("test", .) %>% head(1)
+
+
+
+# Predict test exogenous -------------------------------------------------------
+# Last observation carried forward
+data_locf <- 
+  locf_fun(split_data_cv, c("occ", "ad_diff"), idx_start_test, "locf")
+
 
 
 # Fit models -------------------------------------------------------------------
 # Baseline and ARIMA models
 fit_fable <- 
   split_data_cv %>% 
-  filter(type == "train") %>% 
-  tsibble(index = index, key = c(split, site)) %>% # (for fable models)
+  filter(type == "train" & !is_aggregated(site)) %>% 
+  tsibble(index = index, key = c(split, site)) %>%
   model(
     # Baseline models (for comparison)
-    mean = MEAN(bed_occ),
-    naive = NAIVE(bed_occ),
-    snaive = SNAIVE(bed_occ ~ lag("week")),
+    mean = MEAN(occ),
+    naive = NAIVE(occ),
+    snaive = SNAIVE(occ ~ lag("week")),
     # Arima models
-    arima = ARIMA(bed_occ),
-    arima_d = ARIMA(bed_occ ~ days_),
-    arima_dad = ARIMA(bed_occ ~ days_ + adm + dis),
-    arima_de = ARIMA(bed_occ ~ days_ + bed_escal),
-    arima_dade = ARIMA(bed_occ ~ days_ + adm + dis + bed_escal),
+    arima = ARIMA(occ),
+    arima_dad = ARIMA(occ ~ days_ + ad_diff_f + ad_diff2_f + ad_diff3_f),
+    arima_dad_nof = ARIMA(occ ~ days_ + ad_diff + ad_diff2 + ad_diff3),
+    arima_dad_l = 
+      ARIMA(
+        occ ~ 
+          days_ +
+          
+          ad_diff_f + ad_diff2_f + ad_diff3_f +
+          ad_diff_f_lag1 + ad_diff2_f_lag1 + ad_diff3_f_lag1 +
+          ad_diff_f_lag2 + ad_diff2_f_lag2 + ad_diff3_f_lag2 +
+          ad_diff_f_lag3 + ad_diff2_f_lag3 + ad_diff3_f_lag3 +
+          ad_diff_f_lag4 + ad_diff2_f_lag4 + ad_diff3_f_lag4 +
+          ad_diff_f_lag5 + ad_diff2_f_lag5 + ad_diff3_f_lag5 +
+          ad_diff_f_lag6 + ad_diff2_f_lag6 + ad_diff3_f_lag6 +
+          ad_diff_f_lag7 + ad_diff2_f_lag7 + ad_diff3_f_lag7
+      ),
+    arima_dado = 
+      ARIMA(
+        occ ~ days_ + ad_diff_f + ad_diff2_f + ad_diff3_f + occ_other
+      ), 
+    arima_dado_l = 
+      ARIMA(
+        occ ~ 
+          days_ + 
+          ad_diff_f + ad_diff2_f + ad_diff3_f + occ_other +
+          ad_diff_f_lag1 + ad_diff2_f_lag1 + ad_diff3_f_lag1 + occ_other_lag1 +
+          ad_diff_f_lag2 + ad_diff2_f_lag2 + ad_diff3_f_lag2 + occ_other_lag2 +
+          ad_diff_f_lag3 + ad_diff2_f_lag3 + ad_diff3_f_lag3 + occ_other_lag3 +
+          ad_diff_f_lag4 + ad_diff2_f_lag4 + ad_diff3_f_lag4 + occ_other_lag4 +
+          ad_diff_f_lag5 + ad_diff2_f_lag5 + ad_diff3_f_lag5 + occ_other_lag5 +
+          ad_diff_f_lag6 + ad_diff2_f_lag6 + ad_diff3_f_lag6 + occ_other_lag6 +
+          ad_diff_f_lag7 + ad_diff2_f_lag7 + ad_diff3_f_lag7 + occ_other_lag7
+      )
   )
+
+
+# ARIMA aggregated (exclude occ_other!)
+fit_fable_agg <- 
+  split_data_cv %>% 
+  filter(type == "train") %>%
+  tsibble(index = index, key = c(split, site)) %>%
+  model(
+    arima_dad_agg = 
+      ARIMA(
+        occ ~ 
+          days_ + 
+          ad_diff_f + ad_diff2_f + ad_diff3_f
+      ),
+    arima_dad_l_nof_agg = 
+      ARIMA(
+        occ ~ 
+          days_ + 
+          ad_diff + ad_diff2 + ad_diff3 +
+          ad_diff_lag1 + ad_diff2_lag1 + ad_diff3_lag1 +
+          ad_diff_lag2 + ad_diff2_lag2 + ad_diff3_lag2 +
+          ad_diff_lag3 + ad_diff2_lag3 + ad_diff3_lag3 +
+          ad_diff_lag4 + ad_diff2_lag4 + ad_diff3_lag4 +
+          ad_diff_lag5 + ad_diff2_lag5 + ad_diff3_lag5 +
+          ad_diff_lag6 + ad_diff2_lag6 + ad_diff3_lag6 +
+          ad_diff_lag7 + ad_diff2_lag7 + ad_diff3_lag7
+      )
+  )
+
+
+# Vector autoregressive models
+fit_fable_var_ad <- # adm - dis
+  split_data_cv %>% 
+  filter(type == "train", !is_aggregated(site)) %>%
+  mutate(site = site %>% as.character()) %>% 
+  tsibble(index = index, key = c(split, site)) %>%
+  model(
+    var_ad = VAR(vars(occ, ad_diff_f))
+  )
+
+fit_fable_var_ad2 <- # diff(adm - dis)
+  split_data_cv %>% 
+  filter(type == "train", !is_aggregated(site)) %>%
+  mutate(site = site %>% as.character()) %>% 
+  tsibble(index = index, key = c(split, site)) %>%
+  model(
+    var_ad2 = VAR(vars(occ, ad_diff2_f))
+  )
+
+fit_fable_var_ad3 <- # diff(diff(adm - dis)
+  split_data_cv %>% 
+  filter(type == "train", !is_aggregated(site)) %>%
+  mutate(site = site %>% as.character()) %>% 
+  tsibble(index = index, key = c(split, site)) %>%
+  model(
+    var_ad3 = VAR(vars(occ, ad_diff3_f))
+  )
+
+fit_fable_var_other <- # BRI vs Southmead
+  split_data_cv %>% 
+  filter(type == "train", !is_aggregated(site)) %>%
+  mutate(site = site %>% as.character()) %>% 
+  tsibble(index = index, key = c(split, site)) %>%
+  select(occ) %>% 
+  pivot_wider(names_from = site, values_from = occ) %>% 
+  model(
+    var_other = VAR(vars(BRI, Southmead)),
+  )
+
 
 # Exponential smoothing with predictors (bed escalation)
 es_model <- # define esx fit
   function(.data_es) {
-    .data_es %>% as.ts() %>% es(model = "ZXZ",  lags = c(1, 1, 7))
+    .data_es %>% as.ts() %>% 
+      es(model = "ZXZ",  lags = c(1, 1, 7))
   }
 
-list_var_ese <- c("bed_occ", "bed_escal")
+list_var_ese <- 
+  c("occ", 
+    "ad_diff_f", "ad_diff2_f", "ad_diff3_f",
+    "occ_other")
+# list_var_ese <-
+#   split_data_cv %>%
+#   select(
+#     contains("occ"), -occ_other,
+#     matches("ad_diff.*_f") , -ad_diff_f, -ad_diff2_f, -ad_diff3_f,
+#     contains("days_"), -days_) %>%
+#   names()
 
-fit_ese <- # fit ese (exponential smoothing with bed escalation)
-  cv_wrap(split_data_cv, select_training,  es_model, list_var_ese)
+fit_es <- # fit es
+  cv_wrap(
+    split_data_cv %>% filter(type == "train" & !is_aggregated(site)),
+    select_training,  
+    es_model, 
+    list_var_ese)
+
 
 # Random forest
 list_var_rf <-
-  split_data_cv %>% 
-  select(-c(split, type, site, index, t_ax, days_, bed_occ_z)) %>% 
+  split_data_cv %>%
+  select(
+    contains("occ"), -occ_other,
+    matches("ad_diff.*_f"), -ad_diff_f, -ad_diff2_f, -ad_diff3_f,
+    contains("days_"), -days_) %>%
   names()
 
 rf_reg <- 
-  function(.data_rf, .lag = 1, .horizon = 14) {
+  function(.data_rf, .lag = 1, .horizon = horizon) {
     # Fit random forests from lagged variables and generate forecasts. Use
     # predictors of increasing lag to predict bed occupancy with increasing time
-    # horizon .h. This is equivalent to last observation carried forward, where
-    # which observation is used depends on .lag: .lag = 1 equivalent to last 
-    # day; if .lag > 1 use several days.
+    # horizon .h.
     
     # Train set
     data_train = 
       .data_rf %>% filter(type == "train")
     y_train = 
-      data_train %>% select(bed_occ)
+      data_train %>% select(occ)
     xl_train = # lagged data
-      data_train %>%  select(-contains("bed_occ_z") & contains("lag"))
+      data_train %>%  select(contains("lag"))
     xd_train = # days
       data_train %>% select(starts_with("days_"))
     
@@ -97,9 +234,9 @@ rf_reg <-
     data_test = 
       .data_rf %>% filter(type == "test")
     y_test = 
-      data_test %>% select(bed_occ)
+      data_test %>% select(occ)
     xl_test = # lagged data 
-      data_test %>%  select(-contains("bed_occ_z") & contains("lag"))
+      data_test %>%  select(contains("lag"))
     xd_test = # days
       data_test %>% select(starts_with("days_"))
     
@@ -115,7 +252,7 @@ rf_reg <-
         slice(.h) # predict only the .h day ahead from last observed .lag days
       
       # Compute
-      tmp_fit = randomForest(bed_occ ~ ., data = tmp_train, ntree = 1000)
+      tmp_fit = randomForest(occ ~ ., data = tmp_train, ntree = 1000)
       tmp_fc = predict(tmp_fit,  tmp_test, predict.all = TRUE)
 
       # Forecast parameters
@@ -132,7 +269,10 @@ rf_reg <-
   }
 
 ls_rf <- # fits + fc + parameters fc distribution
-  cv_wrap(split_data_cv, select_training, rf_reg, list_var_rf, "all")
+  cv_wrap(
+    split_data_cv %>% filter(!is_aggregated(site)), 
+    select_training,  rf_reg,  list_var_rf, "all"
+    )
 
 fit_rf <-  # extract fits
   ls_rf %>% 
@@ -140,6 +280,7 @@ fit_rf <-  # extract fits
         map(.x, ~ # split
               map(.x, ~ # horizon
                     pluck(.x, "fit"))))
+
 ls_par <- # extract parameters fc distribution
   ls_rf %>% 
   map(., ~ # site
@@ -151,9 +292,20 @@ ls_par <- # extract parameters fc distribution
 fit_all = 
   list(
     "fable" = fit_fable,
-    "es_e" = fit_ese,
-    "rf" = fit_rf
+    "fable_agg" = fit_fable_agg,
+    "fable_var_ad" = fit_fable_var_ad,
+    "fable_var_ad2" = fit_fable_var_ad2,
+    "fable_var_ad3" = fit_fable_var_ad3,
+    "fable_var_other" = fit_fable_var_other,
+    "es_ae_f" = fit_es,
+    # "rf_dae_f" = fit_rf,
+    "rf_dae_f_par" = ls_par
     )
+# fit_fable = fit_all$fable
+# fit_fable_agg = fit_all$fable_agg
+# fit_es = fit_all$es_ae_f
+# ls_par = fit_all$rf_dae_f_par
+
 
 
 # Forecast ---------------------------------------------------------------------
@@ -165,22 +317,71 @@ fc_fable <-
              filter(type == "test") %>% 
              tsibble(index = index, key = c(split, site)))
 
+# ARIMA with predicted exogenous
+fc_fable_locf <- 
+  fit_fable %>% 
+  select(contains("arima_da")) %>% 
+  forecast(
+    new_data = 
+      data_locf %>% 
+      filter(type == "test") %>% 
+      tsibble(index = index, key = c(split, site))
+  ) %>% 
+  mutate( # change name locf models
+    .model = paste0("locf_",.model)
+  )
+
+# ARIMA reconciled
+fc_fable_locf_rec <- 
+  fit_fable_agg %>% 
+  reconcile(
+    arima_dad_rec = min_trace(arima_dad_agg, method = "mint_cov"),
+    arima_dad_l_nof_rec = min_trace(arima_dad_l_nof_agg, method = "mint_cov")
+    ) %>% 
+  select(-arima_dad_agg, -arima_dad_l_nof_agg) %>%
+  forecast(
+    new_data = data_locf %>% 
+      filter(type == "test") %>% 
+      tsibble(index = index, key = c(split, site))
+  ) %>% 
+  mutate( # change name locf models
+    .model = paste0("locf_",.model)
+  )
+
+# Vector autoregressive models
+fc_fable_var_da <- 
+  fit_fable_var_ad %>% 
+  forecast(h = horizon)
+
+fc_fable_var_da2 <- 
+  fit_fable_var_ad2 %>% 
+  forecast(h = horizon)
+
+fc_fable_var_da3 <- 
+  fit_fable_var_ad3 %>% 
+  forecast(h = horizon)
+
+fc_fable_var_other <- 
+  fit_fable_var_other %>% 
+  forecast(h = horizon)
+
+
 # Exponential smoothing with predictors (esx)
 select_model <- # select model fit (by site and split)
   function(.data, .site, .split, ...)  {
     # .data is list (site) of list (split) of model fits
     list(
       "model" = .data[[.site]][[.split]],
-      "index" = split_data_cv %>% filter(site == .site, split == .split)
+      "index" = data_locf %>% filter(site == .site, split == .split)
     )
   }
 
 es_forecast <- # define esx forecast
   function(.data) {
     .data$index = # get test data
-      .data$index %>% filter(type =="test")
+      .data$index %>% filter(type == "test")
     tmp_fc = # compute forecasts
-      forecast(.data$model, h = 14,
+      forecast(.data$model, h = horizon,
                interval = "prediction",
                level = .95,
                newdata = .data$index
@@ -197,8 +398,8 @@ es_forecast <- # define esx forecast
         cbind
       ) %>% 
       mutate( # create forecast distribution (assuming nid error)
-        .model = "es_e",
-        bed_occ = dist_normal(mean, sd = (`Upper bound (97.5%)` - mean) / 2),
+        .model = "locf_es_ado_f",
+        occ = dist_normal(mean, sd = (`Upper bound (97.5%)` - mean) / 2),
         .mean = mean, # necessary for fable::autoplot
         mean = NULL,
         `Lower bound (2.5%)` = NULL,
@@ -207,7 +408,7 @@ es_forecast <- # define esx forecast
   }
 
 fc_ese <- # forecast with esx model
-  cv_wrap(fit_ese, select_model, es_forecast)
+  cv_wrap(fit_es, select_model, es_forecast)
 
 fc_ese <- # convert to tsibble
   fc_ese %>% 
@@ -225,13 +426,13 @@ fc_forecast =
     tmp_fc =
       bind_cols(.data$model, .data$index) %>% 
       mutate(
-        .model = "rf",
+        .model = "rf_dado_f",
         .mean = mean, # necessary for fable::autoplot
-        bed_occ = dist_normal(mu = mean, sd = sd),
+        occ = dist_normal(mu = mean, sd = sd),
         mean = NULL,
         sd = NULL
       ) %>%
-      relocate(c(.model, bed_occ, .mean), .after = index)
+      relocate(c(.model, occ, .mean), .after = index)
   }
 
 fc_rf <- 
@@ -245,9 +446,18 @@ fc_rf <- # convert to tsibble
 
 
 # Join fc
-dimnames(fc_ese$bed_occ) <- "bed_occ" # add rowname to match fc_fable
-dimnames(fc_rf$bed_occ) <- "bed_occ"
-fc_all <- list(fc_fable, fc_ese, fc_rf) %>% reduce(bind_rows)
+dimnames(fc_ese$occ) <- "occ" # add rowname to match fc_fable
+dimnames(fc_rf$occ) <- "occ"
+fc_all <- 
+  list(
+    fc_fable, fc_fable_locf, fc_fable_locf_rec, fc_ese, fc_rf) %>% 
+  reduce(bind_rows)
+
+fc_var <- 
+  list(
+    fc_fable_var_da, fc_fable_var_da2, fc_fable_var_da3, fc_fable_var_other
+  )
+
 
 
 # Save fits and forecasts ------------------------------------------------------
@@ -260,3 +470,5 @@ if (!file.exists(save_path)) {
 saveRDS(split_data_cv, file = paste0(save_path, "splits_short.RDS"))
 saveRDS(fit_all, file = paste0(save_path, "fits_short.RDS"))
 saveRDS(fc_all, file = paste0(save_path, "forecasts_short.RDS"))
+saveRDS(fc_var, file = paste0(save_path, "forecasts_short_var.RDS"))
+# fit_all <- readRDS(paste0(save_path, "fits_short.RDS"))
