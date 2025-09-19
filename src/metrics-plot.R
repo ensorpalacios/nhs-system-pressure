acf6c9a3761294e20f9cb7b5fbc7296669b286f0
Ensor Palacios <yq18021@bristol.ac.uk>
2025-09-19 16:47:20 +0100

    modify plot metrics and added metric tables

--- src/metrics-plot.R
+++ src/metrics-plot.R
@@ -23,25 +23,29 @@ metrics_summary <- metric_data$metrics_summary
 sites <- metrics$site %>% unique()
 metric_names <- metrics$metric %>% unique()
 
-# Get subset models
-metrics <- 
-  metrics %>% 
-  filter(!grepl("locf.*_l", models)) # exclude locf lagged models
-  # filter(!grepl("var_(?!ad)", models, perl = TRUE)) # exclude VAR
-
-metrics_summary <- 
-  metrics_summary %>% filter(!grepl("locf.*_l", models))
-
 
 
 # Generate plots ---------------------------------------------------------------
 plt_metric <- # boxplot
   map(metric_names, \(.metric) {
-    metrics %>% 
-      filter(metric == .metric, models != "tslm") %>% 
-      ggplot(aes(x = models, y = value_s)) +
-      geom_boxplot(outliers = FALSE) +
-        theme(axis.text.x = element_text(angle = 45, hjust=1)) +
+    metrics %>%
+      filter(metric == .metric, models != "tslm") %>%
+      ggplot(aes(x = models, y = value_s, colour = models)) +
+      # geom_boxplot(outliers = FALSE) +
+      ggdist::stat_interval(
+        aes(alpha = after_stat(level),),
+        linewidth = 5,
+        point_interval = "median_qi",
+        .width = c(.10, .50, .8)
+        ) +
+      ggdist::stat_pointinterval(
+        color = "black",
+        fatten_point = 1,
+        .width = c(0)) +
+      geom_hline(yintercept = 1, lty = "dotted") +
+      scale_colour_manual(name = "models", values = col_models) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust=1)) +
       facet_wrap(vars(site, penalty), scales = "free_y")
   }) %>%
   set_names(metric_names)
@@ -75,6 +79,104 @@ plt_metric_time_summary <- # time series (summary data)
 
 
 
+# Generate tables --------------------------------------------------------------
+# Get lighter colour palette (alpha = 0.6)
+col_models_l <- col_models
+substr(col_models_l, 8, 9) <- "80"
+
+
+# Models' metric mode and iqr
+tbl_metric <- 
+  metrics %>% # attention: values not scaled by tslm!
+  group_by(site, penalty, metric, models) %>%
+  summarise( # Compute median and iqr
+    median = median(value),
+    iqr = IQR(value)
+  ) %>% 
+  ungroup() %>%
+  mutate( # prepare data for table
+    site = factor(site), 
+    median = sprintf("%.2f", median),
+    iqr = sprintf("%.2f", iqr),
+    `median (iqr)` = str_glue("{median} ({iqr})"),
+    median = NULL,
+    iqr = NULL
+  ) %>% 
+  arrange(metric, penalty) %>% # reorder for hierarchical columns
+  pivot_wider( # convert to wide for hierarchical columns
+    names_from = c(metric, penalty),
+    values_from = `median (iqr)`,
+    names_sep = "-"
+  ) %>%
+  mutate( # reorder by crps-upper
+    ref_median = as.numeric(substr(`crps-upper`, 1, 4)),
+    ref_iqr = as.numeric(substr(`crps-upper`, 7, 10))
+  ) %>% group_by(site) %>% arrange(ref_median, ref_iqr) %>% 
+  ungroup() %>% select(-ref_median, -ref_iqr) %>% 
+  gt(
+    rowname_col = "models", groupname_col = "site", row_group_as_column = TRUE
+  ) %>% 
+  tab_spanner_delim(delim = "-")
+
+for (i in seq_along(col_models_l)) { # add color to stabl column
+  .color = col_models_l[i]
+  .model = names(col_models_l)[i]
+
+  tbl_metric =
+    tbl_metric %>% 
+    tab_style(
+      style = cell_fill(.color),
+      # location = cells_stub(rows = models == .model)
+      location = cells_stub(rows = models == .model[[1]])
+    ) %>% 
+    tab_style(
+      style = cell_fill(.color),
+      location = cells_body(rows = models == .model)
+    )
+}
+
+
+# Models' frequency as top model (scaled metrics)
+tbl_freq_best <- 
+  metrics_summary %>% 
+  select(split, site, penalty, metric, best_model, models) %>% 
+  group_by(site, penalty, metric, models) %>% 
+  summarise(
+    freq = sum(best_model == models)
+  ) %>% ungroup() %>% arrange(metric, penalty) %>% 
+  pivot_wider(
+    names_from = c(metric, penalty), 
+    values_from = freq,
+    names_sep = "-"
+  ) %>% 
+  mutate( # reorder by crps-upper
+    site = factor(site), # required for groupname_col = "site"
+    ref_freq = as.numeric(`crps-upper`),
+  ) %>% group_by(site) %>% arrange(site, -ref_freq) %>% 
+  ungroup() %>% select(-ref_freq) %>%
+  gt(
+    rowname_col = "models", groupname_col = "site", row_group_as_column = TRUE
+  ) %>%
+  tab_spanner_delim(delim = "-") %>% 
+  opt_vertical_padding(scale = 0.65)# %>% 
+
+for (i in seq_along(col_models_l)) { # add raw colours by model
+  .color = col_models_l[i]
+  .model = names(col_models_l)[i]
+  tbl_freq_best =
+    tbl_freq_best %>% 
+    tab_style(
+      style = cell_fill(.color),
+      location = cells_stub(rows = models == .model)
+    ) %>%
+    tab_style(
+      style = cell_fill(.color),
+      location = cells_body(rows = models == .model)
+    )
+}
+
+
+
 # Save plots -------------------------------------------------------------------
 save_path_m <- here("output/plots/metrics/")
 if (!file.exists(save_path_m)) {
@@ -85,23 +187,21 @@ walk(metric_names, \(.metric) {
   plt_metric %>% 
     pluck(.metric) %>% 
     ggsave(
-      file = str_glue("{save_path_m}{.metric}_boxplot.eps"),
+      file = str_glue("{save_path_m}{.metric}_boxplot.svg"),
       width = 15, height = 8.88,
       dpi = 500
     )
   
-  # plt_metric_time %>% 
-  #   pluck(.metric) %>% 
-  #   ggsave(
-  #     file = str_glue("{save_path_m}{.metric}_ts.eps"),
-  #     width = 15, height = 8.88,
-  #     dpi = 500
-  #   )
   plt_metric_time_summary %>% 
     pluck(.metric) %>% 
     ggsave(
-      file = str_glue("{save_path_m}{.metric}_ts_summary.eps"),
+      file = str_glue("{save_path_m}{.metric}_ts_summary.svg"),
       width = 15, height = 8.88,
       dpi = 500
     )
 })
+
+tbl_metric %>% 
+  gtsave(filename = str_glue("{save_path_m}table_metrics.png"))
+tbl_freq_best %>% 
+  gtsave(filename = str_glue("{save_path_m}table_freq_best.png"))
