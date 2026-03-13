a361abb74d40bc7332b330acd869306729965812
Nick Howlett <nhowlett87@gmail.com>
2026-03-13 15:55:34 +0000

    updated prepare data script

--- shinyApp/00_prepare_data.R
+++ shinyApp/00_prepare_data.R
@@ -1,11 +1,18 @@
 library(RMySQL)
 library(dplyr)
+local <- TRUE 
+
+if (local) {
+  conn <- dbConnect(RSQLite::SQLite(), "data/local_db/local_shiny_dev.sqlite")
+  message("Connected to: Local SQLite")
+} else {
 # Write to MYSQL
 # Connection details
 host <- Sys.getenv("DB_HOST")
 dbname <- Sys.getenv("DB_NAME")
 user <- Sys.getenv("DB_USER")
 password <- Sys.getenv("DB_CRED")
+port <- 3306 # Default MySQL port (change if needed)
 
 # Create the connection
 conn <- dbConnect(dbDriver("MySQL"),
@@ -14,6 +21,8 @@ conn <- dbConnect(dbDriver("MySQL"),
                   port = 3306,
                   user = user,
                   password=password)
+  message("Connected to: Hosted SQLite database")
+}
 
 
 model_out <- dbGetQuery(conn, "select * from nhs_bed_pressure") %>%
