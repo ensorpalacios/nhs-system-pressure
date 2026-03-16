4334fc89ba4dff2c4ee217192dcdfbf193dbfb17
Ensor Palacios <yq18021@bristol.ac.uk>
2026-03-16 13:34:27 +0000

    dashboard with predictions and historic ts

--- shinyApp/app.R
+++ shinyApp/app.R
@@ -11,23 +11,23 @@ library(ggplot2)
 library(here)
 library(targets)
 library(htmltools)
-
 source(here("shinyApp/shiny-functions.R"))
 
 source("00_prepare_data.R")
-# model_out <- readRDS(here("target/data/output/2026-02-18.RDS"))
-# model_out$hist <- 
-#   as.data.table(
-#     list(
-#       "site"=rep(c("BRI", "Southmead"), each=14),
-#       "index" = seq.Date(as.Date("2025-11-02")-14, as.Date("2025-11-02")-1),
-#       "occ" = 
-#         c(mean(mean(as.data.table(model_out$fc)[site == "BRI" & .model == "arima_dadp_rec", occ])) + rnorm(14, sd=5),
-#         mean(mean(as.data.table(model_out$fc)[site == "Southmead" & .model == "arima_dadp_rec", occ])) + rnorm(14, sd=5))
-#     )
-#   )
+# source("shinyApp/00_prepare_data.R")
 
-ui <- page_fillable(
+ui <- page_sidebar(
+  # Sidebar panel for inputs
+  sidebar = sidebar(
+    # Main navigation
+    radioButtons(
+      "main_nav",
+      "View",
+      choices = list("Risk" = "risk", "Data" = "data"),
+      selected = "risk"
+    )
+  ),
+
   # Theme
   title = "",
   theme = bs_theme(
@@ -35,52 +35,132 @@ ui <- page_fillable(
     fg = "#000000" # Foreground (text) color
   ),
 
+  conditionalPanel(
+    condition = "input.main_nav == 'risk'",
+   layout_columns(
+      col_widths = c(8, 4),
+      titlePanel("7 days ahead risk predictions"),
+      div(
+        style = "margin-top: 20px;",
+        selectInput(
+          "select_risk",
+          "Risk type:",
+          choices = list(
+            "Daily risk" = "daily risk",
+            "Daily & aggregate risk" = "daily + aggregate"
+          ),
+          selected = "daily risk",
+          width = "100%"
+        )
+      )
+    ), 
 
-  titlePanel("7 days ahead risk predictions"),
-
-  layout_columns(
-    col_widths = c(7, 5),
-    div(
-      strong("Bed occupancy forecast"),
-      tooltip(
-        bs_icon("info-circle"),
-        "2 weeks bed occupancy history (black line), 1 week ahead forecast 
+    layout_columns(
+      col_widths = c(7, 5),
+      div(
+        strong("Bed occupancy forecast"),
+        tooltip(
+          bs_icon("info-circle"),
+          "2 weeks bed occupancy history (black line), 1 week ahead forecast 
         (mean, 50%, 80% prediction interval), and bed occupancy threshold 
-        (dotted line)",
-      ) #,
-      # style = "margin-bottom: 10px; margin-top: 10px; font-size: 16px;"
-    ),
-    div(
-      strong("Risk predictions"),
-      tooltip(
-        bs_icon("info-circle"),
-        "probability of bed occupancy forecast crossing the threshold
+        (dotted line)"
+        ) #,
+        # style = "margin-bottom: 10px; margin-top: 10px; font-size: 16px;"
+      ),
+      div(
+        strong("Risk predictions"),
+        tooltip(
+          bs_icon("info-circle"),
+          "probability of bed occupancy forecast crossing the threshold
         by days and at least once for day aggregates (first 3, last 4 days, 
         whole week ahead)"
-      ) #,
-      # style = "margin-bottom: 10px; margin-top: 10px; font-size: 16px;"
+        ) #,
+        # style = "margin-bottom: 10px; margin-top: 10px; font-size: 16px;"
+      )
     ),
-  ),
     # BRI card
     card(
       card_header(strong("BRI")),
       card_body(
         layout_columns(
           col_widths = c(7, 5),
-          plotOutput("fc_bri", height = "300px"),
-          plotOutput("bri_risk", height = "300px")
+          div(
+            style = "height: 300px;",
+            plotOutput("fc_bri", height = "300px")
+          ),
+          div(
+            style = "height: 300px;",
+            conditionalPanel(
+              condition = "input.select_risk == 'daily risk'",
+              plotOutput("bri_risk_d", height = "300px")
+            ),
+            conditionalPanel(
+              condition = "input.select_risk == 'daily + aggregate'",
+              plotOutput("bri_risk_a", height = "300px")
+            )
+          )
         )
       )
     ),
 
-  # Southmead card
-  card(
-    card_header(strong("Southmead")),
-    card_body(
-      layout_columns(
-        col_widths = c(7, 5),
-        plotOutput("fc_southmead", height = "300px"),
-        plotOutput("southmead_risk", height = "300px")
+    # Southmead card
+    card(
+      card_header(strong("Southmead")),
+      card_body(
+        layout_columns(
+          col_widths = c(7, 5),
+          div(
+            style = "height: 300px;",
+            plotOutput("fc_southmead", height = "300px")
+          ),
+          div(
+            style = "height: 300px;",
+            conditionalPanel(
+              condition = "input.select_risk == 'daily risk'",
+              plotOutput("southmead_risk_d", height = "300px")
+            ),
+            conditionalPanel(
+              condition = "input.select_risk == 'daily + aggregate'",
+              plotOutput("southmead_risk_a", height = "300px")
+            )
+          )
+        )
+      )
+    )
+  ),
+
+  # Data Page
+  conditionalPanel(
+    condition = "input.main_nav == 'data'",
+   layout_columns(
+      col_widths = c(8, 4),
+      titlePanel("Data View"),
+      div(
+        style = "margin-top: 20px;",
+        selectInput(
+          "data_ts",
+          "Variable:",
+          choices = list(
+            "Bed occupancy" = "occ",
+            "Admission and discharges" = "ad_diff",
+            "Paediatric A&E" = "paed",
+            "Length of stay" = "los",
+            "Temperature" = "temp"
+          ),
+          selected = "occ",
+          width = "100%"
+        )
+      )
+    ), 
+
+    card(
+      card_header(strong("BRI")),
+      card_body(
+          plotOutput("data_bri", height = "300px")
+      ),
+      card_header(strong("Southmead")),
+      card_body(
+          plotOutput("data_southmead", height = "300px")
       )
     )
   )
@@ -91,30 +171,88 @@ server <- function(input, output) {
   model <- "equal"
 
   # Get data
-  risk_d <- as.data.table(model_out)[type == "risk_d" & .model == model, .(site, index, risk_day)]
-  risk_ws_close <- as.data.table(model_out)[type == "risk_ws" & .model == model & week_split == "close", .(site, risk_ws)]
-  risk_ws_far <- as.data.table(model_out)[type == "risk_ws" & .model == model & week_split == "far", .(site, risk_ws)]
-  risk_w <- as.data.table(model_out)[type == "risk_w" & .model == model, .(site, risk_w)]
+  risk_d <- as.data.table(model_out)[
+    type == "risk_d" & .model == model,
+    .(site, index, risk_day)
+  ]
+  risk_ws_close <- as.data.table(model_out)[
+    type == "risk_ws" & .model == model & week_split == "close",
+    .(site, risk_ws)
+  ]
+  risk_ws_far <- as.data.table(model_out)[
+    type == "risk_ws" & .model == model & week_split == "far",
+    .(site, risk_ws)
+  ]
+  risk_w <- as.data.table(model_out)[
+    type == "risk_w" & .model == model,
+    .(site, risk_w)
+  ]
   fc <- as.data.table(model_out)[type == "forecast" & .model == model]
-  thr <- as.data.table(model_out)[type == "risk_w" & .model == model, .(site, thr)]
+  thr <- as.data.table(model_out)[
+    type == "risk_w" & .model == model,
+    .(site, thr)
+  ]
   hist <- as.data.table(historic_data)
 
-  # Plot risk
-  output$bri_risk <- renderPlot({
-    plot_riskd(risk_d, risk_ws_close, risk_ws_far, risk_w, "BRI")
+  # Plot risk - daily
+  output$bri_risk_d <- renderPlot({
+    plot_riskd(
+      risk_d, 
+      risk_ws_close, 
+      risk_ws_far, 
+      risk_w, 
+      "BRI", 
+      "daily risk"
+    )
+  })
+  output$southmead_risk_d <- renderPlot({
+    plot_riskd(
+      risk_d,
+      risk_ws_close,
+      risk_ws_far,
+      risk_w,
+      "Southmead",
+      "daily risk"
+    )
   })
 
-  output$southmead_risk <- renderPlot({
-    plot_riskd(risk_d, risk_ws_close, risk_ws_far, risk_w, "Southmead")
+  # plot risk - daily + aggregate
+  output$bri_risk_a <- renderPlot({
+    plot_riskd(
+      risk_d,
+      risk_ws_close,
+      risk_ws_far,
+      risk_w,
+      "BRI",
+      "daily + aggregate"
+    )
+  })
+  output$southmead_risk_a <- renderPlot({
+    plot_riskd(
+      risk_d,
+      risk_ws_close,
+      risk_ws_far,
+      risk_w,
+      "Southmead",
+      "daily + aggregate"
+    )
   })
 
-  # Plot bed occupancy
+  # Plot bed occupancy fc
   output$fc_bri <- renderPlot(
-    plot_fc(fc, hist, thr, "BRI", "forecast")
+    plot_fc(fc, hist, thr, "BRI")
   )
 
   output$fc_southmead <- renderPlot(
-    plot_fc(fc, hist, thr, "Southmead", "forecast")
+    plot_fc(fc, hist, thr, "Southmead")
+  )
+
+  # Plot data time series
+  output$data_bri <- renderPlot(
+    plot_ts(hist, "BRI", input$data_ts)
+  )
+  output$data_southmead <- renderPlot(
+    plot_ts(hist, "Southmead", input$data_ts)
   )
 }
 
--- shinyApp/shiny-functions.R
+++ shinyApp/shiny-functions.R
@@ -10,81 +10,148 @@
 #' @param .fc Forecasts
 #' @param .thr Threshold for high system pressure
 #' @param .site Site to display
-#' @param .hist Historical data??
-plot_fc <- function(.fc, .hist, .thr, .site, .type) {
+#' @param .hist Historical data
+plot_fc <- function(.fc, .hist, .thr, .site) {
   .fc <- .fc[site == .site]
   .hist <- .hist[site == .site]
   .thr <- .thr[site == .site, thr]
 
-  if (.type == "forecast") {
-    .hist = .hist[index >= max(index) - lubridate::dweeks(2), ]
-    compute_quantiles <- function(q, .data) {
-      quantile(.data, p = c(q))
-    }
-    .fc[, occ := dist_normal(occ_mean, sqrt(occ_var))]
-    .fc[,
-      # get percentiles
-      c("10%", "25%", "75%", "90%") := lapply(
-        c(0.1, 0.25, 0.75, .9),
-        compute_quantiles,
-        .data = .SD[, occ]
-      )
-    ]
+  .hist = .hist[index >= max(index) - lubridate::dweeks(2), ]
+  compute_quantiles <- function(q, .data) {
+    quantile(.data, p = c(q))
+  }
+  .fc[, occ := dist_normal(occ_mean, sqrt(occ_var))]
+  .fc[,
+    # get percentiles
+    c("10%", "25%", "75%", "90%") := lapply(
+      c(0.1, 0.25, 0.75, .9),
+      compute_quantiles,
+      .data = .SD[, occ]
+    )
+  ]
 
-    # Plot
-    .fc |>
-      ggplot(aes(x = index, y = mean(occ))) +
+  # Plot
+  .fc |>
+    ggplot(aes(x = index, y = mean(occ))) +
+    geom_line() +
+    geom_ribbon(
+      aes(ymin = `25%`, ymax = `75%`, fill = "50%"),
+      alpha = 0.3
+    ) +
+    geom_ribbon(
+      aes(ymin = `10%`, ymax = `90%`, fill = "80%"),
+      alpha = 0.1
+    ) +
+    geom_hline(
+      aes(yintercept = .thr),
+      colour = "red",
+      linetype = "dotted",
+      linewidth = 1.5
+    ) +
+    geom_line(data = .hist, aes(x = index, y = occ)) +
+    # scale_y_continuous(name = "none") +
+    scale_x_date(
+      labels = \(x) {
+        format(x, "%a\n%d-%m")
+      },
+      breaks = "2 day",
+      name = "bed occupancy"
+    ) +
+    scale_fill_manual(
+      name = "Forecast\ninterval",
+      values = c("50%" = "steelblue", "80%" = "steelblue")
+    ) +
+    # facet_wrap(vars(site), ncol = 1, scales = "free_y") +
+    theme(
+      # axis.title.y = element_text(size = 14),
+      axis.title.y = element_blank(),
+      axis.title.x = element_blank(),
+      axis.text = element_text(size = 14),
+      # strip.text = element_text(size = 24),
+      strip.text = element_blank(),
+      strip.background = element_rect(fill = "white", linewidth = 0),
+      legend.position = "none"
+      # legend.text = element_text(size = 14),
+      # legend.title = element_text(size = 14, margin = margin(b = 15)),
+      # legend.margin = margin(t = 5, r = 4, b = 5, l = 4),
+      # legend.background = element_rect(colour = "black", linewidth = 1)
+    )
+}
+
+
+#' Time series plot
+#' Function for plotting training data time series
+#' @param .hist Historical data
+#' @param .site Site to display
+#' @param .var Variable to plot (character)
+plot_ts <- function(.hist, .site, .var) {
+  if (.var != "temp") {
+    tmp_ts <- .hist[site == .site, .SD, .SDcols = c("index", .var)]
+    tmp_ts <- tmp_ts[index >= max(index) - lubridate::dweeks(16)]
+    tmp_ts |>
+      ggplot(aes(x = index, y = !!sym(.var))) +
       geom_line() +
-      geom_ribbon(
-        aes(ymin = `25%`, ymax = `75%`, fill = "50%"),
-        alpha = 0.3
-      ) +
-      geom_ribbon(
-        aes(ymin = `10%`, ymax = `90%`, fill = "80%"),
-        alpha = 0.1
-      ) +
-      geom_hline(
-        aes(yintercept = .thr),
-        colour = "red",
-        linetype = "dotted",
-        linewidth = 1.5
-      ) +
-      geom_line(data = .hist, aes(x = index, y = occ)) +
-      # scale_y_continuous(name = "none") +
-      scale_x_date(
-        labels = \(x) {
-          format(x, "%a\n%d-%m")
-        },
-        breaks = "2 day",
-        name = "bed occupancy"
-      ) +
-      scale_fill_manual(
-        name = "Forecast\ninterval",
-        values = c("50%" = "steelblue", "80%" = "steelblue")
-      ) +
-      # facet_wrap(vars(site), ncol = 1, scales = "free_y") +
+      theme_minimal() +
       theme(
-        # axis.title.y = element_text(size = 14),
-        axis.title.y = element_blank(),
-        axis.title.x = element_blank(),
-        axis.text = element_text(size = 14),
-        # strip.text = element_text(size = 24),
-        strip.text = element_blank(),
-        strip.background = element_rect(fill = "white", linewidth = 0),
-        legend.position = "none"
-        # legend.text = element_text(size = 14),
-        # legend.title = element_text(size = 14, margin = margin(b = 15)),
-        # legend.margin = margin(t = 5, r = 4, b = 5, l = 4),
-        # legend.background = element_rect(colour = "black", linewidth = 1)
+        axis.title = element_blank(),
+        axis.text = element_text(size = 14)
       )
+  } else {
+    tmp_ts <- .hist[site == .site, .SD, .SDcols = c("index", "tmax", "tmin")]
+    tmp_ts <- tmp_ts[index >= max(index) - lubridate::dweeks(16)]
+    tmp_plot <-
+      tmp_ts |>
+      ggplot(aes(x = index)) +
+      geom_line(aes(y = tmax, colour = "tmax")) +
+      geom_line(aes(y = tmin, colour = "tmin")) +
+      theme_minimal() +
+      theme(
+        axis.title = element_blank(),
+        axis.text = element_text(size = 14)
+      )
+    if (.site == "BRI") {
+      tmp_plot +
+        theme(
+          legend.title = element_blank(),
+          legend.position = c(0.5, 1),
+          legend.direction = "horizontal",
+          legend.justification = c(0.5, 1),
+          legend.text = element_text(size = 14, colour = "black"),
+          # legend.key.width = unit(2, "cm"),
+          # legend.key.height = unit(0.6, "cm"),
+          # legend.spacing.x = unit(0.5, "cm"),
+          # legend.spacing.y = unit(0.2, "cm"),
+          # legend.margin = margin(t = 5, b = 5),
+          legend.box.background = element_rect(
+            fill = "white",
+            colour = "grey70"
+          )
+        )
+    } else {
+      {
+        tmp_plot +
+          theme(
+            legend.position = "none"
+          )
+      }
+    }
   }
 }
+
+
 
 #' Daily risk plot
 #' Function for plotting aily risk predictions
 #' @param .risk_d Risk predictions
 #' @param .site Site to display
-plot_riskd <- function(.risk_d, .risk_ws_close, .risk_ws_far, .risk_w, .site) {
+plot_riskd <- function(
+  .risk_d,
+  .risk_ws_close,
+  .risk_ws_far,
+  .risk_w,
+  .site,
+  .type
+) {
   # Get data
   .risk_d <- .risk_d[site == .site]
   .risk_ws_close <- .risk_ws_close[site == .site]
@@ -99,26 +166,24 @@ plot_riskd <- function(.risk_d, .risk_ws_close, .risk_ws_far, .risk_w, .site) {
   x_close_l <- .risk_d[2, index]
   x_far_l <- .risk_d[6, index] - 0.5
   x_week_l <- .risk_d[4, index]
-  y_close <- .risk_d[1:3, max(risk_day)] +0.3
+  y_close <- .risk_d[1:3, max(risk_day)] + 0.3
   y_far <- .risk_d[4:7, max(risk_day)] + 0.3
   y_week <- .risk_d[, max(risk_day)] + 0.5
   max_y <- ifelse((y_week + 0.2) > 1, y_week + 0.2, 1)
- 
+
   # Plot
-  .risk_d |>
+  tmp_plot =
+    .risk_d |>
     ggplot(aes(x = index, y = risk_day, fill = risk_day)) +
     geom_col() +
     scale_y_continuous(name = NULL, limits = c(0, max_y), breaks = NULL) +
     scale_x_date(labels = \(x) format(x, "%a\n%d-%m"), breaks = "day") +
     geom_hline(yintercept = 0) + # baseline
-    geom_segment(
-      aes(x = x_close_s, xend = x_close_e, y = y_close, yend = y_close)
-    ) + # 3 days line
-    geom_segment(
-      aes(x = x_far_s, xend = x_far_e, y = y_far, yend = y_far)
-    ) + # 4 days line
-    geom_segment(
-      aes(x = x_close_s, xend = x_far_e, y = y_week, yend = y_week)
+    scale_fill_gradient(
+      low = "#56B1F7",
+      high = "#132B43",
+      limits = c(0, 1),
+      labels = scales::percent
     ) + # week line
     geom_text(
       aes(label = scales::percent(round(risk_day, 2))),
@@ -127,43 +192,6 @@ plot_riskd <- function(.risk_d, .risk_ws_close, .risk_ws_far, .risk_w, .site) {
       # hjust = 0.5,
       vjust = -0.2,
       size = 6
-    ) +
-    annotate(
-      geom = "text",
-      label = sprintf("%.0f%%", .risk_ws_close[, risk_ws] * 100),
-      x = x_close_l,
-      y = y_close,
-      vjust = -0.2,
-      # linewidth = NA,
-      # fill = "white",
-      size = 6
-    ) +
-    annotate(
-      geom = "text",
-      label = sprintf("%.0f%%", .risk_ws_far[, risk_ws] * 100),
-      x = x_far_l,
-      y = y_far,
-      vjust = -0.2,
-      # linewidth = NA,
-      # fill = "text",
-      size = 6
-    ) +
-    annotate(
-      geom = "text",
-      label = sprintf("%.0f%%", .risk_w[, risk_w] * 100),
-      x = x_week_l,
-      y = y_week,
-      vjust = -0.2,
-      # linewidth = NA,
-      # fill = "white",
-      size = 6
-    ) +
-    # facet_wrap(vars(site), ncol = 1) +
-    scale_fill_gradient(
-      low = "#56B1F7",
-      high = "#132B43",
-      limits = c(0, 1),
-      labels = scales::percent
     ) +
     theme(
       axis.title.x = element_blank(),
@@ -175,9 +203,124 @@ plot_riskd <- function(.risk_d, .risk_ws_close, .risk_ws_far, .risk_w, .site) {
       panel.grid.major = element_blank(),
       panel.grid.minor = element_blank(),
       legend.position = "none"
-      # legend.text = element_text(size = 14),
-      # legend.title = element_text(size = 14, margin = margin(b = 15)),
-      # legend.background = element_rect(colour = "black", linewidth = 1),
-      # legend.margin = margin(t = 5, r = 4, b = 10, l = 4)
     )
+
+  if (.type == "daily risk") {
+    tmp_plot
+  } else {
+    tmp_plot +
+      geom_segment(
+        aes(x = x_close_s, xend = x_close_e, y = y_close, yend = y_close)
+      ) + # 3 days line
+      geom_segment(
+        aes(x = x_far_s, xend = x_far_e, y = y_far, yend = y_far)
+      ) + # 4 days line
+      geom_segment(
+        aes(x = x_close_s, xend = x_far_e, y = y_week, yend = y_week)
+      ) +
+      annotate(
+        geom = "text",
+        label = sprintf("%.0f%%", .risk_ws_close[, risk_ws] * 100),
+        x = x_close_l,
+        y = y_close,
+        vjust = -0.2,
+        # linewidth = NA,
+        # fill = "white",
+        size = 6
+      ) +
+      annotate(
+        geom = "text",
+        label = sprintf("%.0f%%", .risk_ws_far[, risk_ws] * 100),
+        x = x_far_l,
+        y = y_far,
+        vjust = -0.2,
+        # linewidth = NA,
+        # fill = "text",
+        size = 6
+      ) +
+      annotate(
+        geom = "text",
+        label = sprintf("%.0f%%", .risk_w[, risk_w] * 100),
+        x = x_week_l,
+        y = y_week,
+        vjust = -0.2,
+        # linewidth = NA,
+        # fill = "white",
+        size = 6
+      )
+  }
+  # .risk_d |>
+  #   ggplot(aes(x = index, y = risk_day, fill = risk_day)) +
+  #   geom_col() +
+  #   scale_y_continuous(name = NULL, limits = c(0, max_y), breaks = NULL) +
+  #   scale_x_date(labels = \(x) format(x, "%a\n%d-%m"), breaks = "day") +
+  #   geom_hline(yintercept = 0) + # baseline
+  # geom_segment(
+  #   aes(x = x_close_s, xend = x_close_e, y = y_close, yend = y_close)
+  # ) + # 3 days line
+  # geom_segment(
+  #   aes(x = x_far_s, xend = x_far_e, y = y_far, yend = y_far)
+  # ) + # 4 days line
+  # geom_segment(
+  #   aes(x = x_close_s, xend = x_far_e, y = y_week, yend = y_week)
+  # ) + # week line
+  # geom_text(
+  #   aes(label = scales::percent(round(risk_day, 2))),
+  #   # linewidth = NA,
+  #   # fill = "white",
+  #   # hjust = 0.5,
+  #   vjust = -0.2,
+  #   size = 6
+  # ) +
+  # annotate(
+  #   geom = "text",
+  #   label = sprintf("%.0f%%", .risk_ws_close[, risk_ws] * 100),
+  #   x = x_close_l,
+  #   y = y_close,
+  #   vjust = -0.2,
+  #   # linewidth = NA,
+  #   # fill = "white",
+  #   size = 6
+  # ) +
+  # annotate(
+  #   geom = "text",
+  #   label = sprintf("%.0f%%", .risk_ws_far[, risk_ws] * 100),
+  #   x = x_far_l,
+  #   y = y_far,
+  #   vjust = -0.2,
+  #   # linewidth = NA,
+  #   # fill = "text",
+  #   size = 6
+  # ) +
+  # annotate(
+  #   geom = "text",
+  #   label = sprintf("%.0f%%", .risk_w[, risk_w] * 100),
+  #   x = x_week_l,
+  #   y = y_week,
+  #   vjust = -0.2,
+  #   # linewidth = NA,
+  #   # fill = "white",
+  #   size = 6
+  # ) +
+  # scale_fill_gradient(
+  #   low = "#56B1F7",
+  #   high = "#132B43",
+  #   limits = c(0, 1),
+  #   labels = scales::percent
+  # ) +
+  # theme(
+  #   axis.title.x = element_blank(),
+  #   axis.text = element_text(size = 14),
+  #   strip.text = element_text(size = 24),
+  #   strip.background = element_rect(fill = "white", linewidth = 0),
+  #   panel.border = element_rect(linewidth = 0),
+  #   panel.background = element_rect(fill = "white"),
+  #   panel.grid.major = element_blank(),
+  #   panel.grid.minor = element_blank(),
+  #   legend.position = "none"
+  #   # legend.text = element_text(size = 14),
+  #   # legend.title = element_text(size = 14, margin = margin(b = 15)),
+  #   # legend.background = element_rect(colour = "black", linewidth = 1),
+  #   # legend.margin = margin(t = 5, r = 4, b = 10, l = 4)
+  # )
 }
